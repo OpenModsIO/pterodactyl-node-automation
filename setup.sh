963f3114bc8f60e6744b35dd34e2db1cd6898150
Matt Withoos <mattwithoos@gmail.com>
2023-03-03 14:27:51 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,81 +1,28 @@
 #!/bin/bash
 
-# Update
+# STEP ONE: Create the instance
+# instance type for tiny is "g6-standard-1", region for sydney is "ap-southeast"
+curl -H "Content-Type: application/json" \
+-H "Authorization: Bearer $TOKEN" \
+-X POST -d '{
+    "image": "linode/ubuntu18.04",
+    "region": "ap-southeast",
+    "type": $INSTANCETYPE,
+    "label": "sydney-t1",
+    "tags": [],
+    "root_pass": $INSTANCE_PASSWORD,
+    "authorized_users": [
+        $USERS
+    ],
+    "booted": true,
+    "backups_enabled": false,
+    "private_ip": false
+}' https://api.linode.com/v4/linode/instances
 
-apt-get update
+# STEP TWO: note down the IP, set up a subdomain on cloudflare, disable proxy, 
 
-apt-get -y upgrade
+# STEP THREE: then do a lazy install
+git clone https://github.com/pterodactyl-installer/pterodactyl-installer
+./install.sh
 
-# UFW
-
-ufw allow ssh comment ssh
-
-ufw enable
-
-ufw allow 8080 comment "Pterodactyl Daemon"
-
-ufw allow 2022 comment "Pterodactyl SFTP"
-
-ufw allow 80 comment "Standalone Webserver For SSL (Using Certbot)"
-
-# Certbot
-
-apt-get -y install certbot
-
-# other dependencies
-
-apt-get -y install unzip tar python
-
-# Docker
-
-curl -sSL https://get.docker.com/ | CHANNEL=stable bash
-
-systemctl enable docker
-
-# Nodejs
-
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-
-apt -y install nodejs make gcc g++
-
-# Pterodactyl Daemon
-
-mkdir -p /srv/daemon /srv/daemon-data
-
-cd /srv/daemon
-
-curl -L https://github.com/pterodactyl/daemon/releases/download/v0.6.12/daemon.tar.gz | tar --strip-components=1 -xzv
-
-npm install --only=production
-
-npm audit fix
-
-# Daemon Service
-
-echo "[Unit]
-
-Description=Pterodactyl Wings Daemon
-
-After=docker.service
-
-[Service]
-
-User=root
-
-#Group=some_group
-
-WorkingDirectory=/srv/daemon
-
-LimitNOFILE=4096
-
-PIDFile=/var/run/wings/daemon.pid
-
-ExecStart=/usr/bin/node /srv/daemon/src/index.js
-
-Restart=on-failure
-
-StartLimitInterval=600
-
-[Install]
-
-WantedBy=multi-user.target" > /etc/systemd/system/wings.service
+# STEP FOUR: Obtain the auto-setup token from the panel and paste it in the CLI of the new node
